103f85baeeea97c01968a90bba24ef2f50cbf26f
Rai Amrit <134118243+Amaricannei@users.noreply.github.com>
2023-11-29 04:06:37 +0800

    WIP error with inout UserName

--- Code Block/script.js
+++ Code Block/script.js
@@ -1,51 +1,55 @@
-let value = 0
-if (value) {
-  console.log("Value1 is truthy");
-} else {
-  console.log("Value1 is falsy");
+var beginning = "Hi! Name Please: ";
+var quiz = "start";
+var UserName ="";
+
+var score = 0;
+
+function randomPic() {
+var calc = Math.floor(Math.random()*50)+1;
+return calc;
 }
 
-let value1 = 1;
-if (value1) {
-  console.log("Value1 is truthy");
-} else {
-  console.log("Value1 is falsy");
-}
+var myOutputValue = "";
 
-let value2 = "";
-if (value2) {
-  console.log("Value2 is truthy");
-} else {
-  console.log("Value2 is falsy");
-}
+var main = function(input) {
 
-let value3 = "Hello";
-if (value3) {
-  console.log("Value3 is truthy");
-} else {
-  console.log("Value3 is falsy");
-}
+  Userinput = input.toLowerCase().trim();
+  var computer = randomPic();
 
-let value4 = null;
-if (value4) {
-  console.log("Value4 is truthy");
-} else {
-  console.log("Value4 is falsy");
-}
+  if (beginning == "Hi! Name Please; ") {
+    UserName = Userinput;
+    quiz = "start";
+    return `${UserName}, your guessing from 1 to 50 time starts now`;
+  } else if (Userinput == "end") {
+    myOutputValue = `${Userinput} See you later.`;
+    console.log(myOutputValue);
+    return;
+    }
+  
+  if (Userinput == "s") {
+    quiz = "start";
+    return `${UserName}, You have entered Start Zone`;
+  } else if (Userinput == "d") {
+    quiz ="double";
+    return `${UserName}, You are entering the section to win double.`
+  }
 
-let value5 = undefined;
-if (value5) {
-  console.log("Value5 is truthy");
-} else {
-  console.log("Value5 is falsy");
-}
+  if ( quiz == "start") {
+    if ((Userinput >=1 && Userinput <=50) &&  Userinput == Computer || Userinput == computer*2) {
+      score++;
+      return `${UserName} it hard to get it correct, you did it!`;
+    } else {
+      return "Your input is incorrect only between 1 to 50"
+    }
+    
+  }if (quiz == "double") {
+    if (Userinput == computer) {
+      score++;
+      score++;
+      return `${UserName} It's more tough to match compensated double`;
+    } else {
+      return "Your input is incorrect only between 1 to 50"
+    }
+  }
 
-let value6 = NaN;
-if (value6) {
-  console.log("Value6 is truthy");
-} else {
-  console.log("Value6 is falsy");
 }
-// need to add Exit 
-// WIn rate Calculation
-//UserName

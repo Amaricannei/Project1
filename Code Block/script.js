25e0b7b93052a5fdebfd78a9cebaca9e09c73a6e
Rai Amrit <134118243+Amaricannei@users.noreply.github.com>
2023-11-28 16:55:32 +0800

    additional code

---
+++ Code Block/script.js
@@ -0,0 +1,51 @@
+let value = 0
+if (value) {
+  console.log("Value1 is truthy");
+} else {
+  console.log("Value1 is falsy");
+}
+
+let value1 = 1;
+if (value1) {
+  console.log("Value1 is truthy");
+} else {
+  console.log("Value1 is falsy");
+}
+
+let value2 = "";
+if (value2) {
+  console.log("Value2 is truthy");
+} else {
+  console.log("Value2 is falsy");
+}
+
+let value3 = "Hello";
+if (value3) {
+  console.log("Value3 is truthy");
+} else {
+  console.log("Value3 is falsy");
+}
+
+let value4 = null;
+if (value4) {
+  console.log("Value4 is truthy");
+} else {
+  console.log("Value4 is falsy");
+}
+
+let value5 = undefined;
+if (value5) {
+  console.log("Value5 is truthy");
+} else {
+  console.log("Value5 is falsy");
+}
+
+let value6 = NaN;
+if (value6) {
+  console.log("Value6 is truthy");
+} else {
+  console.log("Value6 is falsy");
+}
+// need to add Exit 
+// WIn rate Calculation
+//UserName
